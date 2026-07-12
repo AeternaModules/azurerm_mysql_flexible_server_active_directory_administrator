f25666bc95f6fccd9c80bf86c047fc79316a9093
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "mysql_flexible_server_active_directory_administrators" {
-  description = "All mysql_flexible_server_active_directory_administrator resources"
-  value       = azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators
-}
 output "mysql_flexible_server_active_directory_administrators_identity_id" {
-  description = "List of identity_id values across all mysql_flexible_server_active_directory_administrators"
-  value       = [for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : v.identity_id]
+  description = "Map of identity_id values across all mysql_flexible_server_active_directory_administrators, keyed the same as var.mysql_flexible_server_active_directory_administrators"
+  value       = { for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : k => v.identity_id }
 }
 output "mysql_flexible_server_active_directory_administrators_login" {
-  description = "List of login values across all mysql_flexible_server_active_directory_administrators"
-  value       = [for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : v.login]
+  description = "Map of login values across all mysql_flexible_server_active_directory_administrators, keyed the same as var.mysql_flexible_server_active_directory_administrators"
+  value       = { for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : k => v.login }
 }
 output "mysql_flexible_server_active_directory_administrators_object_id" {
-  description = "List of object_id values across all mysql_flexible_server_active_directory_administrators"
-  value       = [for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : v.object_id]
+  description = "Map of object_id values across all mysql_flexible_server_active_directory_administrators, keyed the same as var.mysql_flexible_server_active_directory_administrators"
+  value       = { for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : k => v.object_id }
 }
 output "mysql_flexible_server_active_directory_administrators_server_id" {
-  description = "List of server_id values across all mysql_flexible_server_active_directory_administrators"
-  value       = [for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : v.server_id]
+  description = "Map of server_id values across all mysql_flexible_server_active_directory_administrators, keyed the same as var.mysql_flexible_server_active_directory_administrators"
+  value       = { for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : k => v.server_id }
 }
 output "mysql_flexible_server_active_directory_administrators_tenant_id" {
-  description = "List of tenant_id values across all mysql_flexible_server_active_directory_administrators"
-  value       = [for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : v.tenant_id]
+  description = "Map of tenant_id values across all mysql_flexible_server_active_directory_administrators, keyed the same as var.mysql_flexible_server_active_directory_administrators"
+  value       = { for k, v in azurerm_mysql_flexible_server_active_directory_administrator.mysql_flexible_server_active_directory_administrators : k => v.tenant_id }
 }
 
